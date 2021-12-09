75186fb515c5e2a2f1d5674257464a136f5a9849
Giuseppe Persiano <giuper@gmail.com>
2021-12-09 18:33:10 +0100

    zaino errato

--- 07-Backtrack/README.md
+++ 07-Backtrack/README.md
@@ -11,6 +11,8 @@ Prof. [Giuseppe Persiano](https://giuper.github.io)
 3. [Il backtrack risolve il problema delle N regine](backQueen.py)
 4. [Il problema del target](target.py) ed una [versione](targetEff.py)
 pi&ugrave; efficiente.
+5. [Il problema del sudoku](sudokuW.py) ed una [versione](sudoku.py) non
+funzionante da correggere.
 
 
 
@@ -19,4 +21,7 @@ pi&ugrave; efficiente.
 1. Risolvere il problema delle N super regine che possono attaccare anche facendo la mossa del cavallo.
 2. Target con molteplicit&agrave;. Abbiamo una lista L di interi positivi, un target T ed una lista M di molteplicit&agrave; il cui i-esimo elemento indica il numero massimo di volte che &egrave; possibile usare l'i-esimo elemento di L.
 3. Target senza consecutivi. Non si pu&ograve; raggiungere il target T usando due interi consecutivi di L.
+4. La procedura backtrack individua una soluzione del problema. Modificare la procedura in modo da calcolare tutte le possibilie soluzioni al problema delle N regine.
+5. La procedura backtrack individua una soluzione del problema che si sta
+risolvendo. Modificare la procedura in modo da calcolare la soluzione del problema del target che minimizzi il numero di elementi utilizzati.
 
---
+++ 07-Backtrack/sudokuW.py
@@ -0,0 +1,104 @@
+puzzle=[
+[None,4,None,None,None,None,1,7,9],
+[None,None,2,None,None,8,None,5,4],
+[None,None,6,None,None,5,None,None,8],
+[None,8,None,None,7,None,9,1,None],
+[None,5,None,None,9,None,None,3,None],
+[None,1,9,None,6,None,None,4,None],
+[3,None,None,4,None,None,7,None,None],
+[5,7,None,1,None,None,2,None,None],
+[9,2,8,None,None,None,None,6,None]]
+
+solution=[ 
+[8,4,5,6,3,2,1,7,9],
+[7,3,2,9,1,8,6,5,4],
+[1,9,6,7,4,5,3,2,8],
+[6,8,3,5,7,4,9,1,2],
+[4,5,7,2,9,1,8,3,6],
+[2,1,9,8,6,3,5,4,7],
+[3,6,1,4,2,9,7,8,5],
+[5,7,4,1,8,6,2,9,3],
+[9,2,8,3,5,7,4,6,1]]
+
+from back import BackTrack
+
+class Sudoku(BackTrack):
+
+    def __init__(self,puzzle):
+        self.P=[puzzle[i].copy() for i in range(9)]
+        
+    def isFinal(self,stato):
+        M,row,col=stato
+        return row==9
+
+    def allMoves(self,stato):
+        M,row,col=stato
+        if M[row][col] is None:
+            return range(1,10)
+        else:
+            return [M[row][col]]
+
+    def newStato(self,stato,m):
+        M,row,col=stato
+
+        #non basta fare newM=M.copy()
+        #newM=M non funziona
+        #newM=M.copy() nemmeno
+        #in generale usa deepcopy
+        #qui non c'e' bisogno perche' abbiamo solo due livelli
+
+        newM=[M[i].copy() for i in range(9)]
+        newM[row][col]=m
+        if col==8:
+            row+=1
+            col=0
+        else:
+            col+=1
+        newstato=[newM,row,col]
+        return newstato
+
+    def isAdm(self,stato,m):
+        M,row,col=stato
+
+        if M[row][col] is not None:
+            return True
+
+        #controlla riga row
+        for c in range(9):
+            if M[row][c]==m:
+                return False
+            
+        #controlla colonna col
+        for r in range(9):
+            if M[r][col]==m:
+                return False
+
+        #controlla riquadro
+        ##coordinate della cella in alto a sinistra
+        ##del riquadro cui appartiene (row,col)
+        cornerR=(row//3)*3
+        cornerC=(col//3)*3
+        for r in range(cornerR,cornerR+3):
+            for c in range(cornerC,cornerC+3):
+                if M[r][c]==m:
+                    return False
+
+        return True
+
+            
+    def Solve(self,verbose=False):
+        res=self._Solve([self.P,0,0],verbose)
+        ##prima componente True/False
+        if res[0]:
+            print("Soluzione: ")
+            #seconda componente stato finale
+            #prima componente dello stato finale
+            #tavola totalmente riempita
+            for row in res[1][0]:
+                print(row)
+            print()
+        else:
+            print("Nessuna soluzione")
+
+sudoku=Sudoku(puzzle)
+sudoku.Solve()
